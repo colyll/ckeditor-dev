521029903b373960b3895ece70e823e7a8abb71b
colyll <itsgoodid@gmail.com>
2020-03-15 03:01:58 +0800

    Add: Use plupload to upload file to AliCloud AliOSS and  QiNiu Cloud.

---
+++ plugins/format_buttons/lang/zh-cn.js
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'format_buttons', 'zh-cn', {
+	h1: 'H1 标签',
+  h2: 'H2 标签',
+  h3: 'H3 标签',
+  h4: 'H4 标签',
+  h5: 'H5 标签',
+  h6: 'H6 标签'
+} );
