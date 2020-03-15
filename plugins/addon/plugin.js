481aeac041cf9f17dc9db9552c845f6fb35706cd
colyll <itsgoodid@gmail.com>
2020-03-15 16:50:17 +0800

    Add dede plugins.

---
+++ plugins/addon/plugin.js
@@ -0,0 +1,27 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+/**
+ * @file Image plugin
+ */
+
+CKEDITOR.plugins.add( 'addon',
+{
+	init : function( editor )
+	{
+		// Register the dialog.
+		CKEDITOR.dialog.add('addon', this.path + 'dialogs/addon.js' );
+
+		// Register the command.
+		editor.addCommand('addon', new CKEDITOR.dialogCommand('addon') );
+
+		// Register the toolbar button.
+		editor.ui.addButton( 'addon',
+			{
+				label : '附件',
+				command : 'addon'
+			});
+	}
+} );
---
+++ plugins/dedepage/plugin.js
@@ -0,0 +1,28 @@
+// Register a plugin named "dedepage".
+(function()
+{
+    CKEDITOR.plugins.add( 'dedepage',
+    {
+        init : function( editor )
+        {
+            // Register the command.
+            editor.addCommand( 'dedepage',{
+                exec : function( editor )
+                {
+                    // Create the element that represents a print break.
+                    // alert('dedepageCmd!');
+                    editor.insertHtml("#p#副标题#e#");
+                }
+            });
+            // alert('dedepage!');
+            // Register the toolbar button.
+            editor.ui.addButton( 'MyPage',
+            {
+                label : '插入分页符',
+                command : 'dedepage'
+            });
+            // alert(editor.name);
+        },
+        requires : [ 'fakeobjects' ]
+    });
+})();
